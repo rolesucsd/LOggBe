95060a1dccec3f859b73d6fafae8be77e5cfc951
lloomis14 <lloomis@ucsd.edu>
2023-05-27 14:03:48 -0700

    Comments
    Added comments for both the evolution rates script and faith pd scripts.

--- evolution_rates.py
+++ evolution_rates.py
@@ -1,41 +1,89 @@
+
+#--------------------------------------------------------------
+#Chu lab
+#You already Know What It Is
+#Not Entirely Sure What Goes Here
+#
+#Full Liscense is up your butt and around the corner
+#--------------------------------------------------------------
+
+
+
+
 import os
 from ete3 import Tree
 
+
+"""
+Function to Calculate Branch Length
+
+Parameters
+____________
+
+dir_path: path to tree files, trees should be in newick form
+
+output: location where rates.txt file should written to
+
+Returns
+____________
+
+avg_length: the average length of the branches of a phylogenetic tree, calculated via the phylogenetic distance of the trees
+
+rates.txt: all genes and their respective average branch length written to a .txt file
+"""
+
+
 def calculate_branch_lengths(dir_path, output):
+    """
+    Getting the list of file names
+
+    Create a dataframe for the branch lengths to be saved in 
+    
+    Loop over all the IQTREE outputs as .treefiles and skip anything else
+
+    Get the gene base name by splitting by the first "."
+    """
+
     file_names = os.listdir(dir_path)
-    # List of rates of evolution
+
     rates = []
 
-    # Loop over all the IQTREE output files
     for file_name in file_names:
-        # Skip any non-IQTREE files
         if not file_name.endswith(".treefile"):
             continue
 
-        # Get the basename of the file without the extension
-        basename = file_name.split(".", 1)[0]  # Extract base name up to the first "."
+        basename = file_name.split(".", 1)[0]
 
-        # Load the tree from the IQTREE output file
+        """
+        tree: read in the tree based on dir_path
+
+        total_length: set to 0 then add the length of every node to make total branch length
+
+        num_branches: count the number of branhces / nodes by count the number of descendents
+
+        avg_length: divide the total length by the number of branches to get the average branch length and write it to the rates vector
+        """
+
         tree = Tree(os.path.join(dir_path, file_name))
 
-        # Calculate the total branch length
         total_length = 0
         for node in tree.traverse():
             total_length += node.dist
 
-        # Calculate the number of branches
+
         num_branches = len(tree.get_descendants())
-
-        # Calculate the average branch length
         avg_length = total_length / num_branches
-
-        # Add the rate to the list along with the basename
         rates.append((basename, avg_length))
 
-    # Sort the rates in descending order based on the average branch length
+    """
+    rates: data frame with gene name and average branch length, sort the rates into descending order
+    - Highest phylogenetic distance genes on top
+
+    rates.txt: Write rates to a file based upon path at output
+    """
+    
     rates.sort(key=lambda x: x[1], reverse=True)
 
-    # Write the rates to a file
     with open(f"{output}/rates.txt", "w") as f:
         for rate in rates:
             f.write(f"{rate[0]}\t{rate[1]}\n")
--- faith_pd.py
+++ faith_pd.py
@@ -4,9 +4,27 @@ import subprocess
 from Bio import Phylo
 import sys
 
+"""
+Function to create necessary directatories
+
+Parameters
+____________
+
+output_directory: defined place where faithpdsummaries will be saved
+
+biom_dir: folder where sorting biom tables are created
+fpd_dir: folder where faith pd outputs are stored
+filter_tree_dir: folder where filtered newick trees are stored
+
+
+Returns
+____________
+Three folders in which to save results of faith pd and Biom text files
+
+"""
 
 def create_directories(output_directory):
-    # Create the directories if they don't exist
+    
     biom_dir = os.path.join(output_directory, "Biom")
     fpd_dir = os.path.join(output_directory, "FPD")
     filter_tree_dir = os.path.join(output_directory, "Filter_tree")
@@ -15,54 +33,106 @@ def create_directories(output_directory):
     os.makedirs(fpd_dir, exist_ok=True)
     os.makedirs(filter_tree_dir, exist_ok=True)
 
+
+"""
+Function to run faith pd
+
+Parameters
+____________
+
+output_directory: defined place where faithpdsummaries will be saved
+
+group: folder where sorting biom tables are created
+
+input_directory: folder where faith pd outputs are stored
+
+output_directory: output for faithpd summary
+
+faithpd_test2.sh: command line script that runs faith pd
+
+Returns
+____________
+
+A summary text file of the results
+
+"""
+
+
+
 def faith_pd(input_directory, group, output_directory):
     create_directories(output_directory)
 
     file_names = os.listdir(input_directory)
 
+    """
+    makes a list of file names at the input directory with the .treefile extension
+
+    retrieves the basename by splitting on the first period
+
+    creates a path to the output directories needed 
+    - including the biom.txt file, the .biom file, the _pd.txt file, and the _filter.nwk file
+
+    if in filtering too many sequences were removed then txt_pd_file_path wont exist
+
+    """
+
+
     for file_name in file_names:
-        # Skip any non-IQTREE files
+        
         if not file_name.endswith(".treefile"):
             continue
 
-        # Read the tree from the IQTREE output file
         file_path = os.path.join(input_directory, file_name)
         tree = Phylo.read(file_path, "newick")
 
-        # Create file paths for output files
-        base_name = file_name.split(".", 1)[0]  # Extract base name up to the first "."
+        base_name = file_name.split(".", 1)[0]
         txt_file_path = os.path.join(output_directory, "Biom", base_name + ".txt")
         biom_file_path = os.path.join(output_directory, "Biom", base_name + ".biom")
         txt_pd_file_path = os.path.join(output_directory, "FPD", base_name + "_pd.txt")
         tree_path = os.path.join(output_directory, "Filter_tree", base_name + "_filter.nwk")
 
+        """
+        if txt_pd_file_path exists then a list of the tip labels is made
+
+        from this the original biom table is read in as a dataframe, then subsetted by the strains in the filtered tree
+
+        saves the new subsetted df to a csv
+
+        additionally the tree is subsetted and written to a .nwk tree
+
+        """
+
+
+
         if os.path.exists(txt_pd_file_path):
             continue
 
-        # Get all the tip labels from the tree
         tip_labels = [clade.name for clade in tree.get_terminals()]
 
-        # Read the group biom file into a DataFrame
         df = pd.read_csv(group, sep="\t", encoding='latin-1')
 
-        # Subset the dataframe to only include sample and fecal
         df = df[["Sample", "Fecal"]]
 
-        # Filter the DataFrame to remove rows with values other than 0 or 1 in the second column
         df = df[df.iloc[:, 1].isin([0, 1])]
 
-        # Subset the DataFrame based on tip labels
         subset_df = df[df['Sample'].isin(tip_labels)]
 
-        # Subset the tree to only include tips present in the 'Sample' column
         subset_tree = tree.common_ancestor(subset_df['Sample'].tolist())
 
-        # Write the subset DataFrame to a .txt file
         subset_df.to_csv(txt_file_path, sep='\t', index=False)
 
-        # Write the subset tree to a new Newick file
-        sys.setrecursionlimit(10**6)  # Increase recursion limit if needed
+        sys.setrecursionlimit(10**6)
         Phylo.write(subset_tree, tree_path, format="newick")
+
+        """
+
+        the faithpd_test2.sh script is then called, creating the .biom file at its new directory
+
+        then the tree for a given tree has faith pd run on it based on the .biom table
+
+        results are saved to txt_pd_file_path
+
+        """
 
         try:
             # Run the subprocess for faithpd_test2.sh
